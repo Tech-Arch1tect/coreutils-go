f50e142d9bc5073c982bd96d2996849c0bf4b650
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-05-18 20:12:48 +0100

    add container which builds & tests GNU coreutils
    The aim for this is to be able to re-use GNU coreutils tests on our binaries

---
+++ testing/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+test_result=$(make check -j "$(nproc)")
+
+declare -A stats
+while read -r _ label count; do
+    label=${label%:}
+    stats[$label]=$count
+done <<< "$(grep -E '^# (TOTAL|PASS|SKIP|XFAIL|FAIL|XPASS|ERROR):' <<< "$test_result")"
+
+TOTAL=${stats[TOTAL]}
+PASS=${stats[PASS]}
+SKIP=${stats[SKIP]}
+XFAIL=${stats[XFAIL]}
+FAIL=${stats[FAIL]}
+XPASS=${stats[XPASS]}
+ERROR=${stats[ERROR]}
+
+echo "Total tests:    $TOTAL"
+echo "Passed:         $PASS"
+echo "Skipped:        $SKIP"
+echo "Expected fail:  $XFAIL"
+echo "Unexpected pass:$XPASS"
+echo "Failures:       $FAIL"
+echo "Errors:         $ERROR"
